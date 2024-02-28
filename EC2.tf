d2dfd688564384bb2745bcc926219646f1cd1c51
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-02-28 12:30:24 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-0c7f9161f8491665f"
+  ami                         = "ami-0208423bef18c17d1"
   instance_type               = "t2.micro"
-  key_name                    = "WP"
+  key_name                    = "stockholm"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
