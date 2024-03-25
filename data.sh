b851f81aabba20cf632d4d8ce0be10a520d143e4
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-03-25 21:57:26 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -8,5 +8,5 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/k-vamc/wordpress1.git 
-cd wordpress
+cd wordpress1
 docker-compose up -d
